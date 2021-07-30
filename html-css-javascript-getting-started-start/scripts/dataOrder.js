2d1f3fe06b7c33af31a287acbcc2fc2f64d242c7
Pericles001 <periclesadjovi@outlook.fr>
2021-07-30 12:57:33 +0100

    user location

--- html-css-javascript-getting-started-start/scripts/dataOrder.js
+++ html-css-javascript-getting-started-start/scripts/dataOrder.js
@@ -1,24 +1,41 @@
-window.addEventListener('DOMContentLoaded', function(e){
-  const order = localStorage.getItem('order');
+window.addEventListener('DOMContentLoaded', function (e) {
+    const order = localStorage.getItem('order');
 
-  if(order){
-    const pieOrder = JSON.parse(order);
+    if (order) {
+        const pieOrder = JSON.parse(order);
 
-    const orderInput = document.querySelector('#pie-order');
-    orderInput.value = order;
-    
-    const pie = document.querySelector('.pie');
+        const locationBox = document.querySelector('#location');
+        let location = {
+            latitude: "unknown",
+            longitude: "unknown"
+        };
 
-    const title = pie.querySelector('.title');
-    const price = pie.querySelector('.price');
-    const desc  = pie.querySelector('.desc');
+        window.navigator.geolocation.getCurrentPosition(
+            function (position) {
+                location = {
+                    latitude: position.coords.latitude,
+                    longitude:position.coords.longitude
+                }
+             }, function (error) { 
+                 locationBox.value = JSON.stringify(location)
+             });
 
-    title.innerText = pieOrder.title;
-    price.innerText = pieOrder.price;
-    desc.innerText = pieOrder.desc;
 
-    const img = pie.querySelector('img');
-    img.setAttribute('src', `images/${pieOrder.id}.png`);
-    img.setAttribute('alt', pieOrder.title);
-  }
+        const orderInput = document.querySelector('#pie-order');
+        orderInput.value = order;
+
+        const pie = document.querySelector('.pie');
+
+        const title = pie.querySelector('.title');
+        const price = pie.querySelector('.price');
+        const desc = pie.querySelector('.desc');
+
+        title.innerText = pieOrder.title;
+        price.innerText = pieOrder.price;
+        desc.innerText = pieOrder.desc;
+
+        const img = pie.querySelector('img');
+        img.setAttribute('src', `images/${pieOrder.id}.png`);
+        img.setAttribute('alt', pieOrder.title);
+    }
 })
